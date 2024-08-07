9aef41a5a6f333a97b1f206f05e5d869b66cc8cb
Andres Samayoa Penagos <andressamayoap@gmail.com>
2024-08-06 19:54:42 -0600

    feat(add): PatientBasicScreen screen

--- src/components/PatientForm/PatientForm.js
+++ src/components/PatientForm/PatientForm.js
@@ -1,42 +1,62 @@
 import './PatientForm.css';
 
 export default function PatientForm(props) {
+
+    const setNombres = (e) => {
+        props.setNombres(e.target.value)
+    };
+    const setEmail = (e) => {
+        props.setEmail(e.target.value)
+    };
+    const setTelefono = (e) => {
+        props.setTelefono(e.target.value)
+    };
+    const setCui = (e) => {
+        props.setCui(e.target.value)
+    };
+    const setFechaNacimiento = (e) => {
+        props.setFechaNacimiento(e.target.value)
+    };
+    const setSexo = (e) => {
+        props.setSexo(e.target.value)
+    };
+
     return <div className='patientForm'>
         <div className="patientInputs">
             <div className="controlContainer">
                 <span className="controlLabel">Nombre</span>
                 <div className="inputSecundaryContainer">
-                    <input className="textInput" value={props.nombres}/>
+                    <input className="textInput" value={props.nombres} onChange={setNombres}/>
                 </div>
             </div>
             <div className="controlContainer">
                 <span className="controlLabel">Telefono</span>
                 <div className="inputSecundaryContainer">
-                    <input type="text" className="textInput" value={props.telefono}/>
+                    <input type="text" className="textInput" value={props.telefono} onChange={setTelefono}/>
                 </div>
             </div>
             <div className="controlContainer">
                 <span className="controlLabel">Email</span>
                 <div className="inputSecundaryContainer">
-                    <input type="text" className="textInput" value={props.correo}/>
+                    <input type="text" className="textInput" value={props.email} onChange={setEmail}/>
                 </div>
             </div>
             <div className="controlContainer">
                 <span className="controlLabel">CUI</span>
                 <div className="inputSecundaryContainer">
-                    <input type="text" className="textInput" />
+                    <input type="text" className="textInput" value={props.cui} onChange={setCui}/>
                 </div>
             </div>
             <div className="controlContainer">
                 <span className="controlLabel">Fecha de nacimiento</span>
                 <div className="inputSecundaryContainer">
-                    <input className="dateInput" type="date" />
+                    <input className="dateInput" type="date" value={props.fechaNacimiento} onChange={setFechaNacimiento}/>
                 </div>
             </div>
             <div className="controlContainer">
                 <span className="controlLabel">Sexo</span>
                 <div className="inputSecundaryContainer">
-                    <select className="comboBoxInput">
+                    <select className="comboBoxInput" value={props.sexo} onChange={setSexo}>
                         <option>Seleccione uno</option>
                         <option>M</option>
                         <option>F</option>
---
+++ src/screens/PatientBasic/PatientBasicScreen.js
@@ -0,0 +1,77 @@
+import './PatientBasicScreen.css'
+
+import { useState, useEffect } from 'react';
+
+import PatientForm from '../../components/PatientForm/PatientForm';
+import DataTable from '../../components/DataTable/DataTable';
+
+export default function PatientBasicScreen () {
+    const tableColumns = [
+        {field: 'nombres', text: 'Nombres'},
+        {field: 'email', text: 'Email'},
+        {field: 'telefono', text: 'Telefono'},
+        {field: 'cui', text: 'CUI'},
+        {field: 'fecha_nacimiento', text: 'Fecha de nacimiento'},
+        {field: 'edad', text: 'Edad'},
+        {field: 'medico_cabecera', text: 'Medico de cabecera'},
+        {field: 'acciones', text: 'Acciones'}
+    ];
+
+    const cancelarForm = () => {
+        console.log('Limpio')
+        setNombres('')
+        setEmail('')
+        setTelefono('')
+        setCui('')
+        setFechaNacimiento('')
+        setSexo('')
+    };
+    
+    const guardarForm = () => {
+        console.log(nombres,email,telefono,cui,fechaNacimiento,sexo);
+    };
+
+    const eliminarItem = (clienteId) => {
+        console.log(clienteId)
+    };
+
+    const loadTableData = () => {
+        setTableData([]);
+    };
+
+    const [tableData, setTableData] = useState([]);
+    const [clienteId, setClienteId] = useState('');
+    const [nombres, setNombres] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [cui, setCui] = useState('');
+    const [fechaNacimiento, setFechaNacimiento] = useState('');
+    const [sexo, setSexo] = useState('');
+
+    useEffect(loadTableData, []);
+
+    return <div className="PatientBasicScreen">
+        <div className="TitleContainer">
+            <h1>Pacientes</h1>
+        </div>
+        <PatientForm 
+            cancelarFn={cancelarForm}
+            guardarFn={guardarForm}
+
+            setNombres={setNombres}
+            setEmail={setEmail}
+            setTelefono={setTelefono}
+            setCui={setCui}
+            setFechaNacimiento={setFechaNacimiento}
+            setSexo={setSexo}
+
+            nombres={nombres}
+            email={email}
+            telefono={telefono}
+            cui={cui}
+            fechaNacimiento={fechaNacimiento}
+            sexo={sexo}
+        />
+        <DataTable headers={tableColumns} rows={tableData} />
+    </div>
+};
