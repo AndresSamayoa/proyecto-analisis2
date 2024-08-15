80358557f9f7434d88ff0b4acbd44211580e96a5
Andres Samayoa Penagos <andressamayoap@gmail.com>
2024-08-15 16:12:18 -0600

    feat(add): vitalSigns screen

---
+++ src/components/VitalSignsForm/VitalSignsForm.js
@@ -0,0 +1,103 @@
+import './VitalSignsForm.css';
+
+export default function VitalSignsForm(props) {
+
+    const setPresionArterial = (e) => {
+        props.setPresionArterial(e.target.value)
+    };
+
+    const setTemperatura = (e) => {
+        props.setTemperatura(e.target.value)
+    };
+
+    const setFrecuenciaCardiaca = (e) => {
+        props.setFrecuenciaCardiaca(e.target.value)
+    };
+
+    const setRespiraciones = (e) => {
+        props.setRespiraciones(e.target.value)
+    };
+
+    const setSo2 = (e) => {
+        props.setSo2(e.target.value)
+    };
+
+    const setGlucosa = (e) => {
+        props.setGlucosa(e.target.value)
+    };
+
+    const setPeso = (e) => {
+        props.setPeso(e.target.value)
+    };
+
+    const setTalla = (e) => {
+        props.setTalla(e.target.value)
+    };
+
+    const setMasaCorporal = (e) => {
+        props.setMasaCorporal(e.target.value)
+    };
+
+    return <div className='vitalSignsForm'>
+        <div className="userInputs">
+            <div className="controlContainer">
+                <span className="controlLabel">Presion arterial</span>
+                <div className="inputSecundaryContainer">
+                    <input className="textInput" value={props.presionArterial} onChange={setPresionArterial}/>
+                </div>
+            </div>
+            <div className="controlContainer">
+                <span className="controlLabel">Temperatura (C)</span>
+                <div className="inputSecundaryContainer">
+                    <input type="text" className="textInput" value={props.temperatura} onChange={setTemperatura}/>
+                </div>
+            </div>
+            <div className="controlContainer">
+                <span className="controlLabel">Frecuencia Cardíaca</span>
+                <div className="inputSecundaryContainer">
+                    <input type="text" className="textInput" value={props.frecuenciaCardiaca} onChange={setFrecuenciaCardiaca}/>
+                </div>
+            </div>
+            <div className="controlContainer">
+                <span className="controlLabel">Respiraciones por minuto</span>
+                <div className="inputSecundaryContainer">
+                    <input type="text" className="textInput" value={props.respiraciones} onChange={setRespiraciones}/>
+                </div>
+            </div>
+            <div className="controlContainer">
+                <span className="controlLabel">SO2</span>
+                <div className="inputSecundaryContainer">
+                    <input type="text" className="textInput" value={props.so2} onChange={setSo2}/>
+                </div>
+            </div>
+            <div className="controlContainer">
+                <span className="controlLabel">Glucosa</span>
+                <div className="inputSecundaryContainer">
+                    <input type="text" className="textInput" value={props.glucosa} onChange={setGlucosa}/>
+                </div>
+            </div>
+            <div className="controlContainer">
+                <span className="controlLabel">Peso en Lbs</span>
+                <div className="inputSecundaryContainer">
+                    <input type="text" className="textInput" value={props.peso} onChange={setPeso}/>
+                </div>
+            </div>
+            <div className="controlContainer">
+                <span className="controlLabel">Talla en m</span>
+                <div className="inputSecundaryContainer">
+                    <input type="text" className="textInput" value={props.talla} onChange={setTalla}/>
+                </div>
+            </div>
+            <div className="controlContainer">
+                <span className="controlLabel">Indice de masa corporal (%)</span>
+                <div className="inputSecundaryContainer">
+                    <input type="text" className="textInput" value={props.masaCorporal} onChange={setMasaCorporal}/>
+                </div>
+            </div>
+        </div>
+        <div className="vitalSignsFormControls">
+            <div className="guardarBtn" onClick={props.guardarFn}>Guardar</div>
+            <div className="cancelarBtn" onClick={props.cancelarFn}>Cancelar</div>
+        </div>
+    </div>
+};
---
+++ src/screens/VitalSignsScreen/VitalSignsScreen.js
@@ -0,0 +1,66 @@
+import './VitalSignsScreen.css';
+
+import { useState } from 'react';
+
+import VitalSignsForm from '../../components/VitalSignsForm/VitalSignsForm';
+
+
+export default function VitalSignsScreen (props) {
+
+    const cancelarForm = () => {
+        setPresionArterial('');
+        setTemperatura('');
+        setFrecuenciaCardiaca('');
+        setRespiraciones('');
+        setSo2('');
+        setGlucosa('');
+        setPeso('');
+        setTalla('');
+        setMasaCorporal('');
+        console.log('Limpio');
+    };
+    
+    const guardarForm = () => {
+        props.saveVitalSigns(presionArterial,temperatura,frecuenciaCardiaca,respiraciones,so2,glucosa,peso,talla,masaCorporal);
+    };
+
+    const [presionArterial, setPresionArterial] = useState('');
+    const [temperatura, setTemperatura] = useState('');
+    const [frecuenciaCardiaca, setFrecuenciaCardiaca] = useState('');
+    const [respiraciones, setRespiraciones] = useState('');
+    const [so2, setSo2] = useState('');
+    const [glucosa, setGlucosa] = useState('');
+    const [peso, setPeso] = useState('');
+    const [talla, setTalla] = useState('');
+    const [masaCorporal, setMasaCorporal] = useState('');
+    
+    return <div className='VitalSignsBasicScreen'>
+        <div className="TitleContainer">
+            <h1>Signos vitales</h1>
+        </div>
+        <VitalSignsForm 
+            cancelarFn={cancelarForm}
+            guardarFn={guardarForm}
+
+            presionArterial={presionArterial}
+            temperatura={temperatura}
+            frecuenciaCardiaca={frecuenciaCardiaca}
+            respiraciones={respiraciones}
+            so2={so2}
+            glucosa={glucosa}
+            peso={peso}
+            talla={talla}
+            masaCorporal={masaCorporal}
+
+            setPresionArterial={setPresionArterial}
+            setTemperatura={setTemperatura}
+            setFrecuenciaCardiaca={setFrecuenciaCardiaca}
+            setRespiraciones={setRespiraciones}
+            setSo2={setSo2}
+            setGlucosa={setGlucosa}
+            setPeso={setPeso}
+            setTalla={setTalla}
+            setMasaCorporal={setMasaCorporal}
+        />
+    </div>
+}
